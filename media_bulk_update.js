f2ea16c6295f5d8c0963586bcdc34c1ab1778a10
Marcelo Vani <marcellovani@yahoo.co.uk>
2014-11-14 11:20:12 +0000

    Only add the button to wysiwyg using plain text

--- media_bulk_update.js
+++ media_bulk_update.js
@@ -3,7 +3,7 @@
 Drupal.behaviors.media_bulk_update = {
   attach: function(context) {
       // Input types supported.
-      var input_types = ["input", "textarea"]
+      var input_types = ["input", "textarea"];
 
       $(input_types).each(function(key, type) {
           // Append a button to each input type supported.
@@ -14,12 +14,14 @@ Drupal.behaviors.media_bulk_update = {
               fieldContainerClassList = fieldContainerClassList.match(/field-name-([^\s]+)/);
               var fieldClass = fieldContainerClassList[0];
 
-              //@TODO only add the button to fields that have plain text as format
-              
-              // Store the id of the source, so that we know where to copy the text from.
-              // Store the class of the field, so we can update all instances of the same field.
-              $(this).after('<button class="button-apply-all form-submit" data-source="#' +
-                this.id + '" data-dest=".' + fieldClass + '">' + Drupal.t('Copy to all') + '</button>');
+              // Add the button only to fields that have 'plain text' as format.
+              var filter_format = $(this).parents(".text-format-wrapper").find(".filter-list").val();
+              if (typeof(filter_format) == 'undefined' || filter_format == 'plain_text') {
+                  // Store the id of the source, so that we know where to copy the text from.
+                  // Store the class of the field, so we can update all instances of the same field.
+                  $(this).after('<button class="button-apply-all form-submit" data-source="#' +
+                  this.id + '" data-dest=".' + fieldClass + '">' + Drupal.t('Copy to all') + '</button>');
+              }
           });
       });
 
@@ -34,7 +36,6 @@ Drupal.behaviors.media_bulk_update = {
           $(input_types).each(function(key, type) {
               $(dest_selector).find(type + '[id^="edit-field"').val($(source_selector).val()).fadeTo(100, 0.1).fadeTo(300, 1.0);
           });
-
           //@TODO process Drupal.wysiwyg.instances
 
       });
