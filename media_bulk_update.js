80580a26ccc519d4568c655f4ec1cc0502c5a5fb
cornel <hugronaphor@gmail.com>
2014-12-22 15:44:55 +0000

    Support only text fields for edit.

--- media_bulk_update.js
+++ media_bulk_update.js
@@ -11,16 +11,18 @@
           $(this).css({'width': '87%'});
 
           var fieldContainerClassList = $(this).parents('.form-wrapper').attr('class');
-          fieldContainerClassList = fieldContainerClassList.match(/field-name-([^\s]+)/);
-          var fieldClass = fieldContainerClassList[0];
+          if (typeof fieldContainerClassList !== 'undefined' && fieldContainerClassList != '') {
+            fieldContainerClassList = fieldContainerClassList.match(/field-name-([^\s]+)/);
+            var fieldClass = fieldContainerClassList[0];
 
-          // Add the button only to fields that have 'plain text' as format.
-          var filter_format = $(this).parents(".text-format-wrapper").find(".filter-list").val();
-          if (typeof (filter_format) == 'undefined' || filter_format == 'plain_text') {
-            // Store the id of the source, so that we know where to copy the text from.
-            // Store the class of the field, so we can update all instances of the same field.
-            $(this).after('<button class="button-apply-all form-submit" data-source="#' +
-                    this.id + '" data-dest=".' + fieldClass + '">' + Drupal.t('Copy to all') + '</button>');
+            // Add the button only to fields that have 'plain text' as format.
+            var filter_format = $(this).parents(".text-format-wrapper").find(".filter-list").val();
+            if (typeof (filter_format) == 'undefined' || filter_format == 'plain_text') {
+              // Store the id of the source, so that we know where to copy the text from.
+              // Store the class of the field, so we can update all instances of the same field.
+              $(this).after('<button class="button-apply-all form-submit" data-source="#' +
+                      this.id + '" data-dest=".' + fieldClass + '">' + Drupal.t('Copy to all') + '</button>');
+            }
           }
         });
       });
@@ -34,7 +36,7 @@
 
         // Update each field.
         $(input_types).each(function(key, type) {
-          $(dest_selector).find(type + '[id^="edit-field"').val($(source_selector).val()).fadeTo(100, 0.1).fadeTo(300, 1.0);
+          $(dest_selector).find(type + '[id^="edit-field"').val($(source_selector).val()).fadeTo(100, 0.1);
         });
         //@TODO process Drupal.wysiwyg.instances
 
